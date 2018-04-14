b3b405cfce99cab754ed4db300a34eee781075e4
sqlch <sqlchan@163.com>
2018-04-14 20:50:21 +0800

    Initial commit

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,116 @@
+package com.controller;
+
+import com.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+import com.service.WendaService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+@Controller
+public class IndexController {
+    private static final Logger logger= LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    WendaService wendaService;
+
+    //hello
+    @ResponseBody
+    @RequestMapping("/")
+    public String index(){
+        logger.info("index");
+        return "hello "+wendaService.getmessage();
+    }
+
+    //pathvariable,RequestParam
+    @RequestMapping("/profile/{userid}")
+    @ResponseBody
+    public String profile(@PathVariable("userid") int userid,
+                          @RequestParam(value = "name",defaultValue = "llf",required = false) String name){
+        return "profile username is : "+name+", userid is : "+userid;
+    }
+
+    @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
+    public String template(Model model) {
+        model.addAttribute("value1", "vvvvv1");
+        List<String> colors = Arrays.asList(new String[]{"RED", "GREEN", "BLUE"});
+        model.addAttribute("colors", colors);
+
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 4; ++i) {
+            map.put(String.valueOf(i), String.valueOf(i * i));
+        }
+        model.addAttribute("map", map);
+        model.addAttribute("user", new User("LEE"));
+        return "home";
+    }
+
+    @RequestMapping(path = {"/request"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String request(Model model, HttpServletResponse response,
+                          HttpServletRequest request,
+                          HttpSession httpSession,
+                          @CookieValue("JSESSIONID") String sessionId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("COOKIEVALUE:" + sessionId);
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name + ":" + request.getHeader(name) + "<br>");
+        }
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                sb.append("Cookie:" + cookie.getName() + " value:" + cookie.getValue());
+            }
+        }
+        sb.append(request.getMethod() + "<br>");
+        sb.append(request.getQueryString() + "<br>");
+        sb.append(request.getPathInfo() + "<br>");
+        sb.append(request.getRequestURI() + "<br>");
+
+        response.addHeader("nowcoderId", "hello");
+        response.addCookie(new Cookie("username", "nowcoder"));
+
+        return sb.toString();
+    }
+
+    @RequestMapping(path = {"/redirect/{code}"}, method = {RequestMethod.GET})
+    public RedirectView redirect(@PathVariable("code") int code,
+                                 HttpSession httpSession) {
+        httpSession.setAttribute("msg", "jump from redirect");
+        RedirectView red = new RedirectView("/", true);
+        if (code == 301) {
+            red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+        return  red;
+    }
+
+    @RequestMapping(path = {"/admin"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String admin(@RequestParam("key") String key) {
+        if ("admin".equals(key)) {
+            return "hello admin";
+        }
+        throw  new IllegalArgumentException("参数不对");
+    }
+
+    @ExceptionHandler()
+    @ResponseBody
+    public String error(Exception e) {
+        return "error:" + e.getMessage();
+    }
+}
+
---
+++ src/main/java/com/service/WendaService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+@Service
+public class WendaService {
+    public String getmessage(){
+        return "wendaservice";
+    }
+}
